4368b2558ba539166b9697add54775f7aa3dcb42
Tomás Fernández Murga <106606402+tommyfm123@users.noreply.github.com>
2024-04-29 08:22:05 -0300

    cambio en ruta

--- src/Components/Pages/Home/ManerasAyudar.jsx
+++ src/Components/Pages/Home/ManerasAyudar.jsx
@@ -6,10 +6,10 @@ class ManerasAyudar extends Component {
     const opcionesDeAyuda = [
       { titulo: "Dinero", descripcion: "Dona dinero. Ayuda causas importantes.", url: "/dinero" },
       { titulo: "Comida", descripcion: "Dona comida. Alimenta a quienes lo necesitan.", url: "/comida" },
-      { titulo: "Hogar", descripcion: "Dona electrodomesticos y muebles", url: "/hogar" },
-      { titulo: "Escolar", descripcion: "Dona material escolar. Facilita el acceso a la educación.", url: "/escolar" },
-      { titulo: "Vestimenta", descripcion: "Dona ropa. Ayuda a personas a vestirse adecuadamente.", url: "/ropa" },
-      { titulo: "Asistencia", descripcion: "Proporciona asistencia directa a personas necesitadas.", url: "/asistencia" },
+      { titulo: "Hogar", descripcion: "Dona electrodomesticos y muebles. Ayuda a quienes mas necesitan.", url: "/hogar" },
+      // { titulo: "Escolar", descripcion: "Dona material escolar. Facilita el acceso a la educación.", url: "/escolar" },
+      // { titulo: "Vestimenta", descripcion: "Dona ropa. Ayuda a personas a vestirse adecuadamente.", url: "/ropa" },
+      // { titulo: "Asistencia", descripcion: "Proporciona asistencia directa a personas necesitadas.", url: "/asistencia" },
       // Agrega más opciones de ayuda aquí según sea necesario
     ];
 
