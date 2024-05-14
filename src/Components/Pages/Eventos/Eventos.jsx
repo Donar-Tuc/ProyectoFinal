ad452ff3c9e3cefd7aa69b020df409dd9703dfe4
Tomás Fernández Murga <106606402+tommyfm123@users.noreply.github.com>
2024-05-13 21:30:05 -0300

    cambios en form

--- src/Components/Pages/Eventos/Eventos.jsx
+++ src/Components/Pages/Eventos/Eventos.jsx
@@ -82,7 +82,7 @@ const data = [
     },
     {
         imagen: Conin,
-        titulo: "FundacionConin",
+        titulo: "Fundacion Conin",
         etiquetas: ["Dinero"],
         horario: "9am - 4pm de lunes a viernes",
         descripcion: "El día 3 de julio estaremos en la Facultad de Ingeniería de la UNSTA a las 15 horas recibiendo donaciones de comidas no perecederas y leche descremada.", url: "/conin"
