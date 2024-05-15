bd723cc398b58d422c59b152af91fafd80f6a376
Tomás Fernández Murga <106606402+tommyfm123@users.noreply.github.com>
2024-05-15 18:25:06 -0300

    conin texto

--- src/Components/Pages/Categorias/Perfiles/Conin.jsx
+++ src/Components/Pages/Categorias/Perfiles/Conin.jsx
@@ -16,7 +16,7 @@ class ConinPerfil extends Component {
         mapaBoton: "https://maps.app.goo.gl/h17JQ8o4JwF2jvHi7",
         email: "info@conin.org.ar",
         mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.972985500553!2d-65.24260112439718!3d-26.80898878887824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225cff0a67b301%3A0x9cd53b5b2dee2359!2sConin%20Tucum%C3%A1n!5e0!3m2!1sen!2sar!4v1714084489347!5m2!1sen!2sar",  
-        descripcion: '¡Bienvenidos a FANN, la Fundación Ayuda al Niño Necesitado! Desde 1987, nos dedicamos a resolver problemas sociales en Tucumán, enfocándonos en niños, adolescentes y jóvenes en condiciones humildes y sus familias. Nuestro objetivo es brindarles atención integral y tratamiento. Algunos puntos importantes son: asistimos a niños y sus familias en situaciones difíciles, trabajamos con grupos vulnerables como los afectados por violencia o adicciones, ayudamos a organizar y fortalecer a las familias, y promovemos cambios positivos para que puedan participar activamente en la comunidad. En FANN, creemos en garantizar los derechos de los niños, proporcionando recursos materiales y humanos. Nuestro espacio es accesible y ofrece apoyo para el desarrollo físico, mental y social, así como necesidades básicas como alimentación y educación.',
+        descripcion: ' Somos el centro CONIN S.M Tucumán. Existimos para quebrar la desnutrición infantil en nuestra provincia. Formamos parte de la FAMILIA CONIN, fundada por el Dr. Abel Albino en 1993. Hoy en día existen 110 centros franquiciados. Juntos atendemos a más de 4000 niños por semana y hemos recuperado a más de 30500  de la desnutrici',
       },
       
     ];
