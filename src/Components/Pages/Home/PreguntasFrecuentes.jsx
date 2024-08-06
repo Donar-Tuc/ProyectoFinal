47e2483a132d0e1629e8d9b720644742ecf4e936
Martin Fort <martinfort13@gmail.com>
2024-08-06 12:48:26 -0300

    Cambios en Preguntas Frecuentes

--- src/Components/Pages/Home/PreguntasFrecuentes.jsx
+++ src/Components/Pages/Home/PreguntasFrecuentes.jsx
@@ -32,7 +32,7 @@ function AlwaysOpenExample() {
               nuestro formulario de registro con la información necesaria sobre
               tu fundación. Una vez que verifiquemos los datos, te enviaremos
               las credenciales de acceso a tu nueva cuenta. Puedes empezar el
-              proceso aquí.
+              proceso <a href="/registrarse">aquí</a>.
             </Accordion.Body>
           </Accordion.Item>
 
@@ -57,9 +57,7 @@ function AlwaysOpenExample() {
               No, no es necesario crear una cuenta para donar a través de
               DON.AR. Los donantes pueden buscar organizaciones, seleccionar
               categorías de donación, y realizar contribuciones sin necesidad de
-              registro. Sin embargo, crear una cuenta te permitirá seguir tu
-              historial de donaciones y recibir actualizaciones de tus
-              organizaciones favoritas.
+              registro.
             </Accordion.Body>
           </Accordion.Item>
 
@@ -84,31 +82,18 @@ function AlwaysOpenExample() {
               nuestra integración con Mercado Pago. No necesitas tener una
               cuenta en nuestra aplicación para donar, pero se te redirigirá a
               Mercado Pago para completar la transacción. Es importante que
-              tengas una cuenta con ellos para realizar el pago.
+              tengas una cuenta en su plataforma para realizar el pago.
             </Accordion.Body>
           </Accordion.Item>
 
           <Accordion.Item eventKey="6">
-            <Accordion.Header>
-              ¿Cómo puedo encontrar fundaciones cerca de mí?
-            </Accordion.Header>
-            <Accordion.Body>
-              Para encontrar fundaciones cerca de tu ubicación, simplemente
-              permite que nuestra aplicación acceda a tu ubicación GPS. Luego,
-              escoge una categoría de doncación, o bien, ve a “Ver todas las
-              categorías“. Los resultados de búsqueda estarán ordenados en base
-              a tu ubicación.
-            </Accordion.Body>
-          </Accordion.Item>
-
-          <Accordion.Item eventKey="7">
             <Accordion.Header>
               ¿Cómo funcionan los eventos publicados por las fundaciones?
             </Accordion.Header>
             <Accordion.Body>
               Las fundaciones pueden publicar eventos en la plataforma para
               recaudar fondos o reunir voluntarios. Estos eventos están listados
-              en nuestra sección de "Eventos", donde puedes encontrar
+              en nuestra sección de <a href="/eventos">Eventos</a>, donde puedes encontrar
               información detallada sobre cómo participar y contribuir. Una vez
               que un evento ha finalizado, su publicación se eliminará
               automáticamente.
